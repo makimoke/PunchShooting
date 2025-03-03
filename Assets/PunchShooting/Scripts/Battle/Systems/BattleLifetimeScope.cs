080bff4b3979a23c86af1c4a96aa43926c6d28ce
makimoke <maki_k5@yahoo.co.jp>
2025-03-03 14:49:18 +0900

    register PlayerShip

--- Assets/PunchShooting/Scripts/Battle/Systems/BattleLifetimeScope.cs
+++ Assets/PunchShooting/Scripts/Battle/Systems/BattleLifetimeScope.cs
@@ -1,4 +1,7 @@
+using PunchShooting.Battle.Data;
+using PunchShooting.Battle.Scenes;
 using PunchShooting.Battle.Views;
+using PunchShooting.Battle.Views.Player;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using VContainer;
@@ -8,13 +11,20 @@ namespace PunchShooting.Battle.Systems
 {
     public class BattleLifetimeScope : LifetimeScope
     {
+        [SerializeField] private BattleScene battleScene;
         [SerializeField] private PlayerInput playerInput;
         [SerializeField] private BattleFieldView battleFieldView;
 
         protected override void Configure(IContainerBuilder builder)
         {
+            builder.RegisterComponent(battleScene);
             builder.RegisterComponent(playerInput);
             builder.RegisterComponent(battleFieldView);
+            builder.Register<PlayerResourceProvider>(Lifetime.Singleton);
+            builder.Register<PlayerShipViewController>(Lifetime.Singleton);
+            builder.Register<PlayerScoreDataAccessor>(Lifetime.Singleton);
+            builder.Register<PlayerStatusDataAccessor>(Lifetime.Singleton);
+            builder.Register<StageStatusDataAccessor>(Lifetime.Singleton);
         }
     }
 }
