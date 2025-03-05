51f1cd17246de5536ef91b13474adc9585d2c39f
makimoke <maki_k5@yahoo.co.jp>
2025-03-05 18:28:39 +0900

    rename class and remove _bulletObjects

--- Assets/PunchShooting/Scripts/Battle/Views/Player/PlayerBulletViewCreator.cs
+++ Assets/PunchShooting/Scripts/Battle/Views/Player/PlayerBulletViewCreator.cs
@@ -1,5 +1,3 @@
-using System;
-using System.Collections.Generic;
 using PunchShooting.Battle.Definitions.Player;
 using UnityEngine;
 using VContainer;
@@ -7,32 +5,21 @@ using Object = UnityEngine.Object;
 
 namespace PunchShooting.Battle.Views.Player
 {
-    public class PlayerBulletCreator : IDisposable
+    public class PlayerBulletViewCreator
     {
         private readonly BattleFieldView _battleFieldView;
-        private readonly List<GameObject> _bulletObjects = new();
         private readonly PlayerResourceProvider _playerResourceProvider;
         private GameObject _bulletPrefab;
 
         [Inject]
-        public PlayerBulletCreator(BattleFieldView battleFieldView,
+        public PlayerBulletViewCreator(BattleFieldView battleFieldView,
             PlayerResourceProvider playerResourceProvider)
         {
             _battleFieldView = battleFieldView;
             _playerResourceProvider = playerResourceProvider;
         }
 
-        public void Dispose()
-        {
-            DestroyAllBullets();
-        }
-
-        public void Reset()
-        {
-            DestroyAllBullets();
-        }
-
-        public GameObject CreateBullet(PlayerResourceDefinition.PrefabId prefabId, PlayerResourceDefinition.SpriteId spriteId, Vector3 position)
+        public PlayerBulletView CreateBullet(PlayerResourceDefinition.PrefabId prefabId, PlayerResourceDefinition.SpriteId spriteId, Vector3 position)
         {
             if (!_bulletPrefab)
             {
@@ -42,27 +29,10 @@ namespace PunchShooting.Battle.Views.Player
 
             var bulletObject = Object.Instantiate(_bulletPrefab, _battleFieldView.Transform);
             bulletObject.transform.localPosition = position;
-            _bulletObjects.Add(bulletObject);
             var playerBulletView = bulletObject.GetComponent<PlayerBulletView>();
             playerBulletView.SetSprite(_playerResourceProvider.FindSprite(spriteId));
 
-            return bulletObject;
-        }
-
-        public void DestroyBullet(GameObject bulletObject)
-        {
-            _bulletObjects.Remove(bulletObject);
-            Object.Destroy(bulletObject);
-        }
-
-        public void DestroyAllBullets()
-        {
-            foreach (var bullet in _bulletObjects)
-            {
-                Object.Destroy(bullet);
-            }
-
-            _bulletObjects.Clear();
+            return playerBulletView;
         }
     }
 }
