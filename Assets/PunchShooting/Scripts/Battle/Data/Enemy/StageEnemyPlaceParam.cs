20df55f25d7b6e98092f56baef46c9553426bda9
makimoke <maki_k5@yahoo.co.jp>
2025-03-07 17:43:48 +0900

    ステージ敵配置パラメータ追加

---
+++ Assets/PunchShooting/Scripts/Battle/Data/Enemy/StageEnemyPlaceParam.cs
@@ -0,0 +1,30 @@
+using System;
+using UnityEngine;
+
+namespace PunchShooting.Battle.Data.Enemy
+{
+// 敵配置
+    [Serializable]
+    public class EnemyPlaceParam
+    {
+        public string Id;
+        public Vector2 SquarePosition = Vector2.zero; // マス目配置位置
+    }
+
+// 分隊配置
+    [Serializable]
+    public class EnemySquadPlaceParam
+    {
+        public float WaitSeconds; //待ち時間(s)
+        public Vector2 SquarePosition = Vector2.zero; // マス目配置位置
+        public EnemyPlaceParam[] EnemyPlaceParams; // 敵配置
+    }
+
+
+    [CreateAssetMenu(fileName = "so_stage_enemy_place_000", menuName = "PunchShooting/StageEnemyPlaceParam")]
+// ステージ敵配置
+    public class StageEnemyPlaceParam : ScriptableObject
+    {
+        public EnemySquadPlaceParam[] EnemySquadPlaceParams; // 分隊配置
+    }
+}
