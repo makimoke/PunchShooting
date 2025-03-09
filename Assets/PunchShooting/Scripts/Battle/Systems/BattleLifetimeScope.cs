921be5d84647f0e8c26570f97f514550e720de91
makimoke <maki_k5@yahoo.co.jp>
2025-03-09 17:39:33 +0900

    StageEnemyGenerator登録

--- Assets/PunchShooting/Scripts/Battle/Systems/BattleLifetimeScope.cs
+++ Assets/PunchShooting/Scripts/Battle/Systems/BattleLifetimeScope.cs
@@ -6,6 +6,7 @@ using PunchShooting.Battle.Logic.Enemy;
 using PunchShooting.Battle.Logic.Player;
 using PunchShooting.Battle.Scenes;
 using PunchShooting.Battle.Views;
+using PunchShooting.Battle.Views.Enemy;
 using PunchShooting.Battle.Views.Player;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -25,9 +26,14 @@ namespace PunchShooting.Battle.Systems
             builder.RegisterComponent(battleScene);
             builder.RegisterComponent(playerInput);
             builder.RegisterComponent(battleFieldView);
+            //View
             builder.Register<PlayerResourceProvider>(Lifetime.Singleton);
             builder.Register<PlayerShipViewController>(Lifetime.Singleton);
             builder.Register<PlayerBulletViewCreator>(Lifetime.Singleton);
+            builder.Register<EnemyResourceProvider>(Lifetime.Singleton);
+            builder.Register<EnemyViewController>(Lifetime.Singleton);
+            builder.Register<EnemyViewCreator>(Lifetime.Singleton);
+            //Data
             builder.Register<PlayerScoreDataAccessor>(Lifetime.Singleton);
             builder.Register<PlayerStatusDataAccessor>(Lifetime.Singleton);
             builder.Register<PlayerBulletBaseParamDataAccessor>(Lifetime.Singleton);
@@ -35,9 +41,11 @@ namespace PunchShooting.Battle.Systems
             builder.Register<PlayerBulletStatusDataAccessor>(Lifetime.Singleton);
             builder.Register<EnemyStatusDataAccessor>(Lifetime.Singleton);
             builder.Register<EnemyBaseParamDataAccessor>(Lifetime.Singleton);
+            //Logic
             builder.Register<InstanceIdGenerator>(Lifetime.Singleton);
             builder.Register<PlayerBulletStatusLogic>(Lifetime.Singleton);
             builder.Register<EnemyStatusLogic>(Lifetime.Singleton);
+            builder.Register<StageEnemyGenerator>(Lifetime.Singleton);
         }
     }
 }
