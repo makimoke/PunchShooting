6c87f04ccdf7c09199352c09f91312b6829484c0
makimoke <maki_k5@yahoo.co.jp>
2025-03-09 17:56:11 +0900

    SpriteResourceDefinitionに定義を移動

---
+++ Assets/PunchShooting/Scripts/Battle/Difinitions/SpriteResourceDefinition.cs
@@ -0,0 +1,25 @@
+namespace PunchShooting.Battle.Definitions
+{
+    //スプライトリソース定義
+    public static class SpriteResourceDefinition
+    {
+        public enum PrefabId
+        {
+            PShip,
+            PBul,
+            
+            //敵はここから
+            Enemy001=100
+        }
+
+        public enum SpriteId
+        {
+            PShip001,
+            PBul001,
+            PBul002,
+            
+            //敵はここから
+            Enemy001=100
+        }
+    }
+}
