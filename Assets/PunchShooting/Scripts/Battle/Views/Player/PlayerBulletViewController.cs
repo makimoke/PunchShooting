5f3229256983af202166a9e494634f8dc459a0e2
makimoke <maki_k5@yahoo.co.jp>
2025-03-14 18:54:18 +0900

    プレイヤ弾ViewController追加

---
+++ Assets/PunchShooting/Scripts/Battle/Views/Player/PlayerBulletViewController.cs
@@ -0,0 +1,72 @@
+using System;
+using R3;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace PunchShooting.Battle.Views.Player
+{
+    //プレイヤ弾管理
+    public class PlayerBulletViewController : IDisposable
+    {
+        private readonly PlayerBulletView _playerBulletView;
+        public readonly Subject<SpriteCollisionResult> OnCollidedSubject = new(); //敵と衝突した
+
+        private DisposableBag _disposableBag;
+        private SpriteBlinkViewController _spriteBlinkViewController;
+
+        public PlayerBulletViewController(PlayerBulletView playerBulletView)
+        {
+            _playerBulletView = playerBulletView;
+        }
+
+        public bool IsOffScreen { private set; get; } //画面外か？
+        public long InstanceId => _playerBulletView.InstanceId;
+
+        public void Dispose()
+        {
+            _disposableBag.Dispose();
+        }
+
+        public void Initialize()
+        {
+            _playerBulletView.OnTriggerEnterSubject
+                .Subscribe(collider =>
+                {
+                    //敵と衝突時
+                    if (collider.CompareTag("Enemy"))
+                    {
+                        var spriteView = collider.gameObject.GetComponent<BaseSpriteView>();
+                        //Debug.Log($"Bullet:{instanceId} {collisionResult.InstanceId} {spriteView.InstanceId}");
+                        OnCollidedSubject.OnNext(new SpriteCollisionResult(_playerBulletView.InstanceId, spriteView.InstanceId));
+                    }
+                })
+                .AddTo(ref _disposableBag);
+
+            _spriteBlinkViewController = new SpriteBlinkViewController(_playerBulletView.SpriteRenderer);
+        }
+
+        //Viewを破棄
+        public void Destroy()
+        {
+            Object.Destroy(_playerBulletView.gameObject);
+        }
+
+        //毎フレーム呼ばれる
+        public void Update(float deltaTime)
+        {
+            //移動（仮）
+            _playerBulletView.AddPosition(new Vector3(0.0f, 4.0f * deltaTime, 0.0f));
+
+            //画面外判定
+            if (_playerBulletView.Position.y >= 5.40f)
+            {
+                IsOffScreen = true;
+            }
+        }
+
+        public void Blink(Color blinkColor, float blinkSecond)
+        {
+            _spriteBlinkViewController.Blink(blinkColor, blinkSecond);
+        }
+    }
+}
---
+++ Assets/PunchShooting/Scripts/Battle/Views/Player/PlayerBulletsViewController.cs
@@ -0,0 +1,105 @@
+using System;
+using System.Collections.Generic;
+using PunchShooting.Battle.Definitions;
+using R3;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using VContainer;
+
+namespace PunchShooting.Battle.Views.Player
+{
+    //プレイヤ弾管理
+    public class PlayerBulletsViewController : IDisposable
+    {
+        private readonly List<PlayerBulletViewController> _bulletViewControllers = new();
+        private readonly PlayerBulletViewCreator _playerBulletViewCreator;
+        private readonly PlayerInput _playerInput;
+        public readonly Subject<SpriteCollisionResult> OnCollidedBulletSubject = new(); //弾と敵が衝突した
+
+        public readonly Subject<long> OnDestroyedBulletSubject = new();
+
+        private DisposableBag _disposableBag;
+
+
+        [Inject]
+        public PlayerBulletsViewController(PlayerBulletViewCreator playerBulletViewCreator,
+            PlayerInput playerInput)
+        {
+            _playerBulletViewCreator = playerBulletViewCreator;
+            _playerInput = playerInput;
+        }
+
+        public void Dispose()
+        {
+            _disposableBag.Dispose();
+        }
+
+        public void Initialize()
+        {
+        }
+
+        //毎フレーム呼ばれる
+        public void Update(float deltaTime)
+        {
+            //弾移動
+            foreach (var bulletViewController in _bulletViewControllers)
+            {
+                bulletViewController.Update(deltaTime);
+            }
+
+            //画面外削除（逆回し）
+            for (var i = _bulletViewControllers.Count - 1; i >= 0; i--)
+            {
+                var bulletViewController = _bulletViewControllers[i];
+                if (bulletViewController.IsOffScreen)
+                {
+                    OnDestroyedBulletSubject.OnNext(bulletViewController.InstanceId);
+                    bulletViewController.Destroy();
+                    _bulletViewControllers.Remove(bulletViewController);
+                }
+            }
+        }
+
+        public void CreateBullet(long instanceId, Vector3 shipPosition)
+        {
+            var bulletViewController = _playerBulletViewCreator.CreateBullet(instanceId, SpriteResourceDefinition.PrefabId.PBul, SpriteResourceDefinition.SpriteId.PBul001,
+                new Vector3(-0.6f, 0.38f, 0.0f) + shipPosition);
+            _bulletViewControllers.Add(bulletViewController);
+
+            //敵との衝突
+            bulletViewController.OnCollidedSubject
+                .Subscribe(collisionResult => OnCollidedBulletSubject.OnNext(collisionResult))
+                .AddTo(ref _disposableBag);
+        }
+
+        public void DestroyBullet(long instanceId)
+        {
+            var bulletViewController = _bulletViewControllers.Find(item => item.InstanceId == instanceId);
+            if (bulletViewController != null)
+            {
+                bulletViewController.Destroy();
+                _bulletViewControllers.Remove(bulletViewController);
+            }
+        }
+
+        public void DestroyAllBullets()
+        {
+            foreach (var bullet in _bulletViewControllers)
+            {
+                bullet.Destroy();
+            }
+
+            _bulletViewControllers.Clear();
+        }
+
+        //ダメージを受けた
+        public void ReceivedDamage(long instanceId, int value)
+        {
+            var bulletViewController = _bulletViewControllers.Find(bullet => bullet.InstanceId == instanceId);
+            if (bulletViewController != null)
+            {
+                bulletViewController.Blink(Color.red, 0.1f);
+            }
+        }
+    }
+}
