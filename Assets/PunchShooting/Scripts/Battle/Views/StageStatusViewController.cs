946530f29a91195fa8ab9fd6ae2c32a338f543ab
makimoke <maki_k5@yahoo.co.jp>
2025-03-15 10:21:51 +0900

    スコア表示対応

--- Assets/PunchShooting/Scripts/Battle/Views/StageStatusViewController.cs
+++ Assets/PunchShooting/Scripts/Battle/Views/StageStatusViewController.cs
@@ -1,5 +1,6 @@
 using System;
 using PunchShooting.Battle.Data;
+using PunchShooting.Battle.Data.Player;
 using R3;
 using VContainer;
 
@@ -8,15 +9,21 @@ namespace PunchShooting.Battle.Views
     // 画面上情報
     public class StageStatusViewController : IDisposable
     {
-        private DisposableBag _disposableBag;
+        private readonly PlayerScoreDataAccessor _playerScoreDataAccessor;
+        private readonly PlayerStatusDataAccessor _playerStatusDataAccessor;
         private readonly StageStatusDataAccessor _stageStatusDataAccessor;
         private readonly StatusBarView _statusBarView;
+        private DisposableBag _disposableBag;
 
         [Inject]
         public StageStatusViewController(StageStatusDataAccessor stageStatusDataAccessor,
+            PlayerStatusDataAccessor playerStatusDataAccessor,
+            PlayerScoreDataAccessor playerScoreDataAccessor,
             StatusBarView statusBarView)
         {
             _stageStatusDataAccessor = stageStatusDataAccessor;
+            _playerStatusDataAccessor = playerStatusDataAccessor;
+            _playerScoreDataAccessor = playerScoreDataAccessor;
             _statusBarView = statusBarView;
         }
 
@@ -29,6 +36,12 @@ namespace PunchShooting.Battle.Views
         {
             _stageStatusDataAccessor.StatusProperty
                 .Subscribe(status => _statusBarView.SetStageStatus(status))
+                .AddTo(ref _disposableBag);
+            _playerStatusDataAccessor.HpProperty
+                .Subscribe(hp => _statusBarView.SetHp(hp))
+                .AddTo(ref _disposableBag);
+            _playerScoreDataAccessor.ScoreProperty
+                .Subscribe(score => _statusBarView.SetScore(score))
                 .AddTo(ref _disposableBag);
         }
     }
