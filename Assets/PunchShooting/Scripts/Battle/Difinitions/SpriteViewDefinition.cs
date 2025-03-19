5509fb90e1cd8975dcc35eae3fa16b4f4844b68a
makimoke <maki_k5@yahoo.co.jp>
2025-03-19 18:27:38 +0900

    スプライト表示定義追加

---
+++ Assets/PunchShooting/Scripts/Battle/Difinitions/SpriteViewDefinition.cs
@@ -0,0 +1,9 @@
+namespace PunchShooting.Battle.Definitions
+{
+    //スプライト表示定義
+    public static class SpriteViewDefinition
+    {
+        public const float OffscreenTop = 5.4f;
+        public const float OffscreenBottom = -5.4f;
+    }
+}
