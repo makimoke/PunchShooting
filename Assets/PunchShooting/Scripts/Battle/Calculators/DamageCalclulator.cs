bdbd5cacb4e0c954417e668b69bed02d0e18d24c
makimoke <maki_k5@yahoo.co.jp>
2025-03-20 10:47:06 +0900

    コメント追記

--- Assets/PunchShooting/Scripts/Battle/Calculators/DamageCalclulator.cs
+++ Assets/PunchShooting/Scripts/Battle/Calculators/DamageCalclulator.cs
@@ -4,6 +4,7 @@ namespace PunchShooting.Battle.Calculators
 {
     public static class DamageCalculator
     {
+        // TODO: データへの反映はロジックに移動
         // ダメージ加算
         // 戻り値：攻撃値
         public static int AddDamage(ObjectStatus ally, ObjectStatus opponent)
